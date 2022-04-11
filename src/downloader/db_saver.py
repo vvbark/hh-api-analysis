08bd37e5368c2946fffd8d9eb3809d850c0aff49
Ilia Moiseev <villeman.5@yandex.ru>
2022-04-11 21:54:05 +0300

    Integrate parser into mainloop

--- src/downloader/db_saver.py
+++ src/downloader/db_saver.py
@@ -1,10 +1,16 @@
 import logging
 
+from api_response_parser import APIResponseParser
+from config import VACANCY_TYPES
+
 logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 
 class DBSaver:
-    def save_batch(self, batch):
-        logger.info('batch saved to DB')
+    def __init__(self):
+        self.parser = APIResponseParser(VACANCY_TYPES)
 
+    def save_batch(self, batch):
+        batch = self.parser.parse_batch(batch)
+        logger.info('batch saved to DB')
