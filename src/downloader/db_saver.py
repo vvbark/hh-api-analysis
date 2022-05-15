2e5a7d9d5ee739584990eec6bf5a985441ff49c3
Vladimir Barkovskii <vova.raev.2000@mail.ru>
2022-05-15 23:04:19 +0300

    Update db_saver.py

--- src/downloader/db_saver.py
+++ src/downloader/db_saver.py
@@ -73,15 +73,11 @@ class DBSaver:
         batch = self.parser.parse_batch(batch)
         batch = self.check_duplicates(batch)
 
-        current_size = self.db_connection.execute(self.insert_query, batch)
-        self.saved_ids.update(get_ids(batch))
-        logger.info(f'Batch with size {current_size} saved to DB.')
-
-        # try:
-        #     current_size = self.db_connection.execute(self.insert_query, batch)
-        #     self.saved_ids.update(get_ids(batch))
-        #     logger.info(f'Batch with size {current_size} saved to DB.')
-        # except:
-        #     logger.warning(f'Error in inserting. Skipping.')
+        try:
+            current_size = self.db_connection.execute(self.insert_query, batch)
+            self.saved_ids.update(self.get_ids(batch))
+            logger.info(f'Batch with size {current_size} saved to DB.')
+        except:
+            logger.warning(f'Error in inserting. Skipping.')
 
         logger.info(f'--------- There are {len(self.saved_ids)} saved samples now. ---------')
